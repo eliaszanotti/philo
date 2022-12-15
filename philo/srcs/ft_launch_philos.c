a5fc3136dc17497204a1b2e3bf3e8f20d521a4b5
Elias ZANOTTI <zanotti.elias@gmail.com>
2022-12-15 18:07:55 +0100

    add files : add print info in ft_log (ft_error -> ft_log)

--- philo/srcs/ft_launch_philos.c
+++ philo/srcs/ft_launch_philos.c
@@ -12,19 +12,6 @@
 
 #include "philo.h"
 
-void	ft_print_info(t_philo *philo, char *str)
-{
-	long long	first_time;
-	long long	time_diff;
-	t_args		*args;
-
-	args = philo->rules;
-	first_time = args->first_time;
-	time_diff = ft_time_diff(first_time, ft_get_time());
-	printf("\033[1;32m[%lli ms]\033[0m \033[0;32m%d\033[0m %s\n", \
-		time_diff, philo->nb, str);
-}
-
 static void	ft_wait_threads(t_args *args, t_philo **philos)
 {
 	int	i;
