e99baec0ea2ded8f2815787f46cbd29991855c7a
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-03-06 21:04:49 +0100

    change file : change print (color + format)

--- philo/srcs/utils/ft_print_info.c
+++ philo/srcs/utils/ft_print_info.c
@@ -6,7 +6,7 @@
 /*   By: elias <zanotti.elias@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 16:40:13 by elias             #+#    #+#             */
-/*   Updated: 2023/03/06 19:39:04 by ezanotti         ###   ########.fr       */
+/*   Updated: 2023/03/06 21:04:36 by ezanotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ void	ft_print_info(t_philo *philo, char *str)
 	pthread_mutex_lock(&args->printing);
 	if (!args->die)
 	{
-		printf("\033[1;32m[%lli ms]", time_diff);
-		printf("\033[0m \033[0;32m%d\033[0m %s\n", philo->nb + 1, str);
+		printf("\033[1;32m%lli", time_diff);
+		printf("\033[0m \033[0;33m%d\033[0m %s\n", philo->nb + 1, str);
 	}
 	pthread_mutex_unlock(&args->printing);
 }
